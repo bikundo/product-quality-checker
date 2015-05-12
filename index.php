32c138b69970f92f7dc7a76dcf5e2b49998def2e
peter <binmonk@gmail.com>
2015-05-12 11:31:12 +0300

    add composer.json for dependencies

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peter
+ * Date: 5/12/2015
+ * Time: 11:22 AM
+ */
+//require the vendor autoload  file, which auto-loads composer classes(packages).
+require __DIR__.'/vendor/autoload.php';
+
+$mail = new SimpleMail();
+var_dump($mail);
+exit();
