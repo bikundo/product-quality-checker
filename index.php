48a11e5995e0dcf2d4ca030acee91efde20dac89
peter <binmonk@gmail.com>
2015-05-12 16:29:20 +0300

    final logic for checking and sending email results

--- index.php
+++ index.php
@@ -7,13 +7,61 @@
  */
 //require the vendor autoload  file, which auto-loads composer classes(packages).
 require __DIR__ . '/vendor/autoload.php';
+//require the checker class.
 require __DIR__ . '/library/Checker.php';
-$data = [];
-$data['message'] = 'long descriptive body of assessment results';
-$c = new Checker();
-if (Checker::send_email($data)) {
-    echo "sent";
-} else {
-    echo "error!!";
+$response = [];
+if (!isset($_POST['products'])) {
+    $response['success'] = 'false';
+    $response['message'] = 'no products in your request';
+    echo json_encode($response);
+    exit();
 }
+$products_string = $_POST['products'];
+$products = json_decode($products_string);
+$res = quality_check($products);
+//send email with results.
+$message = explode(PHP_EOL, $messages);
+$email = $products->email;
+$c::send_email($message, $email);
+//Json response.
+$response['success'] = 'true';
+$response['message'] = 'products found';
+$response['results'] = $res;
+header('Content-Type: application/json');
+echo json_encode($response);
+
+/**
+ * Run all checks and
+ * @param $item
+ * @return array
+ */
+function quality_check($item)
+{
+    $c = new Checker;
+    $messages = [];
+    $product = $item->product;
+
+//    check slug whitespace
+    if ($c::check_whitespace($product->slug)) {
+        $messages[] = 'There are no whitespaces around slug string.';
+    } else {
+        $messages[] = 'There are whitespaces around slug string.';
+    }
+//    check if product has related images.
+
+    if ($c::check_images($product)) {
+        $messages[] = 'The product has images.';
+    } else {
+        $messages[] = 'The product has no images.';
+    }
+
+//    check required fields
+    $messages[] = array_merge($messages, $c->check_required_fields($product));
+
+    return $messages;
+
+}
+
+
+
 
--- library/Checker.php
+++ library/Checker.php
@@ -1,14 +1,76 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: peter
  * Date: 5/12/2015
  * Time: 12:42 PM
  */
+class Checker
+{
+    /**
+     *
+     */
+    function __construct()
+    {
+        $this->required_fieds = ['description', 'title', 'slug'];
+    }
 
-class Checker {
+    /**
+     * @param $string
+     * @return bool
+     */
+    public static function check_whitespace($string)
+    {
+        if (strpos($string, ' ') > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-    public static function send_email($data)
+    /**
+     * @param $item
+     * @return array
+     */
+    public function check_required_fields($item)
+    {
+        $messages = [];
+        foreach ($this->required_fieds as $field) {
+//            check if product field exists and is not empty.
+            if (array_key_exists($field, $item) && isset($item->$field)) {
+                $messages[] = "product has " . " " . $field;
+            } else {
+                $messages[] = "product does not have  " . " " . $field;
+            }
+        }
+
+        return $messages;
+    }
+
+    /**
+     * @param $item
+     * @return bool
+     */
+    public static function check_images($item)
+    {
+        if (isset($item->images) && !empty($item->images)) {
+//            image(s) exist for this item.
+            return true;
+        } else {
+//            no image for this item.
+            return false;
+        }
+    }
+
+    /**
+     * @param $message
+     * @param $email
+     * @return bool
+     * @throws Exception
+     * @throws phpmailerException
+     */
+    public static function send_email($message, $email)
     {
         //PHPMailer Object
         $mail = new PHPMailer;
@@ -18,16 +80,16 @@ class Checker {
         $mail->FromName = "Full Name";
 
 //To address and name
-        $mail->addAddress("bikundo.peter@gmail.com", "bikundo Peter");
+        $mail->addAddress($email, "Entry Clerk");
 
 //Address to which recipient will reply
-        $mail->addReplyTo("binmonk@gmail.com", "Reply");
+        $mail->addReplyTo("binmonk@gmail.com", "Sent from.");
 
 //Send HTML or Plain Text email
         $mail->isHTML(true);
 
         $mail->Subject = "Quality Check results.";
-        $mail->Body = "<i>".$data['message']."</i>";
+        $mail->Body = "<i>" . $message . "</i>";
 
         if (!$mail->send()) {
             return false;
